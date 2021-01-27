555590e2135c7b3f0c23fc4e1c9507b915b46033
Julian Marx <77610109+julianmarx@users.noreply.github.com>
2021-01-27 08:05:01 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="comp-data",
+    version="0.0.1",
+    author="Julian Marx",
+    author_email="jhw.marx@gmail.com",
+    description='''The comp-data package provides access to the
+                   financial comparables data provided by Aswath
+                   Damodaran via his website.''',
+    url="https://github.com/julianmarx/comp-data",
+    packages=['comp-data'],
+    keywords = ['finance', 'stocks', 'comparables data']
+)
